d627192801f67b4773a581e4980181e09033035d
luedgova <luedgova1967@gmail.com>
2019-09-08 22:57:21 -0500

    Lte Template and PermitsController

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
+/*
+|--------------------------------------------------------------------------
+|
+|Route::get('/', function () {
     return view('welcome');
-});
+ });
+|
+*/
+
+/*RUTAS DE PERMISO*/
+Route::get('permits', 'PermitsController@index');
+
